32c7983b48a6f7768959453b1ce6ac7eb78615ea
flyingman1991 <flyingman1991@outlook.com>
2018-06-23 11:23:45 +0800

    add vector operation file3.cpp

--- file3.cpp
+++ file3.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-   cout << "hello world!" << endl;
+   vector<string> myStr;
+   myStr.push_back("China");
+   myStr.push_back("English");
+   for(auto ite=myStr.begin(); ite!=myStr.end(); ite++)
+   {
+	cout << *ite << " ";
+   }
+   cout << endl << "hello world!" << endl;
    return 0;
 }
